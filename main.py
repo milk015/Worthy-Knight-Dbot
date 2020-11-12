36f4b0c19acd99751a7ee8641b2924febc400d71
Jorge Bejarano <jorgebejarano15@gmail.com>
2020-11-12 08:52:49 -0500

    Work in progress: Poll system 75% vote system in progress

--- main.py
+++ main.py
@@ -51,8 +51,14 @@ async def roll(res, num_of_dice: int):
 
 
 # Work in progress
-@bot.command(name='poll', help='Makes a poll or vote action')
+@bot.command(name='poll', help = 'Makes a poll or vote action. Up to 4 options!(even if you enter more than 4)')
 async def poll_maker(res, num_of_opt: int):
+    # setting limit and ending func if lower than 0
+    if num_of_opt > 4:
+        num_of_opt = 4
+    elif num_of_opt < 0:
+        return await res.send("Enter a number higher than 0 next time....")
+    # function start
     num = str(num_of_opt)
     ask = "You chose " + num + " options. enter the poll options below"
     await res.send(ask)
@@ -68,6 +74,24 @@ async def poll_maker(res, num_of_opt: int):
         counter += 1
 
     print(options)
+    # string clean up, removing init, creating string to print
+    options.pop(0)
+    text_counter = 0
+    vote_text = "Your choices are: "
+    while text_counter != num_of_opt:
+        if text_counter == num_of_opt - 1:
+            vote_text = vote_text + str(text_counter + 1) + ". " + str(options[text_counter])
+        else:
+            vote_text = vote_text + str(text_counter + 1) + ". " + str(options[text_counter]) + ", "
+        text_counter += 1
+    # printing poll options
+    await res.send("you have 60 seconds to vote!!! react with the number you want!!")
+    await res.send(vote_text)
+
+    # accepting votes
+    # reactions to capture :one:, :two:, :three:, :four:
+
+    reactions = [':one:', ':two:', ':three:', ':four']
 
 
 # audio playback
